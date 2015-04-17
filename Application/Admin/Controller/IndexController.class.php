7acd637bc80e9930a0fcf31502ed87bdd0409a59
wanxiangcc <308243528@qq.com>
2015-04-17 16:47:50 +0800

    test

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -1,8 +1,21 @@
 <?php
+
+/**
+ * 后台首页
+ */
 namespace Admin\Controller;
-use Think\Controller;
-class IndexController extends Controller {
-    public function index(){
-    	$this->show();
-    }
+
+use Common\Controller\AdminbaseController;
+
+class IndexController extends AdminbaseController {
+	public function _initialize() {
+		parent::_initialize ();
+		$this->initMenu ();
+	}
+	// 后台框架首页
+	public function index() {
+		$this->assign ( "SUBMENU_CONFIG", json_encode ( D ( "Common/Menu" )->menu_json () ) );
+		$this->display ();
+	}
 }
+
